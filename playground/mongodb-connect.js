7a71c9b5549939defe87d8769b0738e21f8ba27e
Nadish Sood <nadishsood@gmail.com>
2018-12-31 20:13:47 +0530

    Add update script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -11,26 +11,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, client)=>{
 
   const db = client.db('TodoApp');
 
-  // db.collection('todos').insertOne({
-  //   text: 'Something to do',
-  //   completed: false
-  // }, (err, result)=>{
-  //   if(err){
-  //     return console.log('Unable to insert todo', err);
-  //   }
-  //   console.log(JSON.stringify(result.ops, undefined, 2));
-  // })
+  db.collection('todos').insertOne({
+    text: 'Something to do',
+    completed: false
+  }, (err, result)=>{
+    if(err){
+      return console.log('Unable to insert todo', err);
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  })
 
-  // db.collection('users').insertOne({
-  //   name: "Nadish",
-  //   age: 22,
-  //   location: "Pune"
-  // },(err, res)=>{
-  //   if(err){
-  //     return console.log('Unable to insert to users');
-  //   }
-  //   console.log(JSON.stringify(res.ops, undefined, 2));
-  // })
-  //
-  // client.close();
+  db.collection('users').insertOne({
+    name: "Nadish",
+    age: 22,
+    location: "Pune"
+  },(err, res)=>{
+    if(err){
+      return console.log('Unable to insert to users');
+    }
+    console.log(JSON.stringify(res.ops, undefined, 2));
+  })
+
+  client.close();
 });
---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, client)=>{
+  if(err){
+    return console.log('Unable to connect to mongodb server');
+  }
+  console.log('Connected to mongodb server');
+
+  const db = client.db('TodoApp');
+
+  // db.collection('todos').findOneAndUpdate({
+  //   _id: new ObjectID('5c28b479f798c81d8c4d4229')
+  // }, {
+  //   $set: {
+  //     completed: false
+  //   }
+  // },{
+  //   returnOriginal:false
+  // }).then((results)=>{
+  //   console.log(results);
+  // });
+
+  db.collection('users').findOneAndUpdate({
+    _id:new ObjectID('5c29d99a49d96c184079f64c')
+  }, {
+    $set: {name: 'Rahul'},
+    $inc:{age: 1}
+  }, {
+    returnOriginal: false
+  }).then((result)=>{
+    console.log(result);
+  });
+
+
+
+});
